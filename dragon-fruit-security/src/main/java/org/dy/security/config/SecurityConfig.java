5a314470326a30e4ac9ddb7a7a69ebf26493148c
caiwl <caiwenlao@sina.com>
2020-09-11 17:44:07 +0800

    删除不必要的类

---
+++ dragon-fruit-security/src/main/java/org/dy/security/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package org.dy.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * @author caiwl
+ * @date 2020/8/9 11:46
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public JwtAuthenticationTokenFilter authenticationTokenFilterBean(){
+        return new JwtAuthenticationTokenFilter();
+    }
+
+    /**
+     * 配置拦截器保护请求
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("admin")
+                .antMatchers("/vip/**").hasRole("vip")
+                .antMatchers("/user/**").hasRole("user")
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class)
+                .csrf()
+                .disable();
+    }
+
+    /**
+     * 根据自动匹配密码编码器
+     * @return PasswordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
