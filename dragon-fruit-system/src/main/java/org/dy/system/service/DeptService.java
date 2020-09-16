8d372f8515f36f4a2f0383f6e2ef7dd614594e5d
Cai Wenlao <caiwenlao@sina.com>
2020-09-16 22:10:12 +0800

    同步el-admin

--- dragon-fruit-system/src/main/java/org/dy/system/service/DeptService.java
+++
@@ -1,8 +0,0 @@
-package org.dy.system.service;
-
-/**
- * @author caiwl
- * @date 2020/9/13 21:34
- */
-public interface DeptService {
-}
--- dragon-fruit-system/src/main/java/org/dy/system/service/RoleService.java
+++
@@ -1,8 +0,0 @@
-package org.dy.system.service;
-
-/**
- * @author caiwl
- * @date 2020/9/12 11:16
- */
-public interface RoleService {
-}
