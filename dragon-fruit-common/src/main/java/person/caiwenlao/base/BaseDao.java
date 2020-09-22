445a82f5fbd0544c1b7872e02fa227ec942e2883
caiwl <caiwenlao@sina.com>
2020-09-22 16:40:35 +0800

    新增baseDao

---
+++ dragon-fruit-common/src/main/java/person/caiwenlao/base/BaseDao.java
@@ -0,0 +1,51 @@
+package person.caiwenlao.base;
+
+import java.io.Serializable;
+
+/**
+ * @author caiwenlao
+ * @date 2020/9/22 16:38
+ */
+public interface BaseDao<T extends Serializable, PK> {
+    /**
+     * 根据主键删除
+     * @param id 主键
+     * @return 删除数
+     */
+    int deleteByPrimaryKey(PK id);
+
+    /**
+     * 插入
+     * @param record 记录
+     * @return 插入数
+     */
+    int insert(T record);
+
+    /**
+     * 有选择的插入
+     * @param record 记录
+     * @return 插入数
+     */
+    int insertSelective(T record);
+
+    /**
+     * 根据主键查询
+     * @param id id
+     * @return 记录
+     */
+    T selectByPrimaryKey(PK id);
+
+    /**
+     * 有选择的更新
+     * @param record 记录
+     * @return 更新数
+     */
+    int updateByPrimaryKeySelective(T record);
+
+    /**
+     * 更新
+     * @param record 记录
+     * @return 更新数
+     */
+    int updateByPrimaryKey(T record);
+}
